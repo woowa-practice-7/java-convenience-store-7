da98ae5e20219cb47f0515d1bf343468344b31b2
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-12-12 02:10:56 +0900

    feat(model, promotion): Promotion의 일급컬렉션 Promotions 클래스 구현

---
+++ src/main/java/store/model/Promotions.java
@@ -0,0 +1,11 @@
+package store.model;
+
+import java.util.List;
+
+public class Promotions {
+    private final List<Promotion> promotions;
+
+    public Promotions(List<Promotion> promotions) {
+        this.promotions = promotions;
+    }
+}
